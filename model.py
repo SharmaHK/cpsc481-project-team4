fb76f99fbafafd71fbb6dcf9e39e0a89f6f383fd
Coleman Nugent <colemannugent@gmail.com>
2019-03-15 14:18:47 -0700

    model.py: More details

--- model.py
+++ model.py
@@ -8,11 +8,14 @@ class ShipSegment:
 		self.parent.updateHits()
 
 	def __str__(self):
-		return "X" if hit else " "
+		return "X" if self.hit else " "
 
 class WaterSegment:
 	def __init__(self):
 		self.miss = False
+
+	def __str__(self):
+		return "M" if self.miss else " "
 
 class Ship:
 	def __init__(self, size):
@@ -31,15 +34,30 @@ class Ship:
 		if hits == size:
 			self.sunk = True
 
+	def __str__(self):
+		if size == 2:
+			return "Destroyer"
+		elif size == 2:
+			return "Cruiser"
+		elif size == 4:
+			return "Battleship"
+		elif size == 5:
+			return "Carrier"
+		else
+			return "Ship"
 
 class Board:
 	def __init__(self, size=10):
 		self.cells = []
 		for x in range(0, size):
 			temp = []
-			for y in range(0, 2*size):
+			for y in range(0, size):
 				temp.append(None)
 			self.cells.append(temp)
+
+	def addShip(self, x, y, size, orientation):
+		# make sure ship is on the board
+		# make sure the ship does not overlap other ships
 
 class Game:
 	def __init__(self, size=10):
@@ -49,3 +67,5 @@ class Game:
 
 	def gameStart(self):
 		# TODO
+		# add ships
+		# start gameloop
