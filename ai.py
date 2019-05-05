59ac0afe508b46c270eb0cf8092d7763d1bba958
Coleman Nugent <colemannugent@gmail.com>
2019-05-04 20:21:03 -0700

    ai.py: SimpleAI don't try cells twice

--- ai.py
+++ ai.py
@@ -7,7 +7,12 @@ class SimpleAI:
 		self.delay = delay
 
 	def makeMove(self, game):
-		x = random.randint(0, game.size-1)
-		y = random.randint(0, game.size-1)
+		done = False
+		while not done:
+			x = random.randint(0, game.size-1)
+			y = random.randint(0, game.size-1)
+			if not game.humanBoard.at(x, y).beenhit:
+				done = True
+
 		pygame.time.wait(self.delay)
 		return (x, y)
